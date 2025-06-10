003ed98cffc92b654f48e58855de07e4142a4424
JVVS <163188697+joaovictorVsales@users.noreply.github.com>
2025-06-10 16:47:25 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,34 @@
+console.log("Seja bem vindo!!!") 
+ 
+switch(calculadora){
+    case 1:
+        console.log(a+b);
+        break;
+
+    case 2:
+        console.log(a-b)
+        break;
+
+    case 3:
+        console.log(a*b)
+        break;
+
+    case 4:
+         console.log(a/b)
+         break;
+
+    default:
+        console.log("Operação inválida")
+}
+
+
+
+
+
+
+//const soma = (a,b) => a+b
+//const sub = (a,b) => a-b
+//const mult = (a,b) => a*b
+//const div = (a,b) => a/b
+
+
