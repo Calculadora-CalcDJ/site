5948f3a5b115cc25f1495fb089000659bc498af5
JVVS <163188697+joaovictorVsales@users.noreply.github.com>
2025-06-16 00:49:25 -0300

    Adição de laço de repetição
    A variável opção foi movida para dentro do laço para o programa n entrar em loop

--- calc.js
+++ calc.js
@@ -2,9 +2,17 @@ console.log("Seja bem vindo!!!")
 
 let n1 = Number(prompt("Escolha um numero: "));
 let n2 = Number(prompt("Escolha outro numero: "));
-let opcao = Number(prompt("Escolha um recurso: "));
+let opcao;
 
-
+do{
+    console.log("MENU INTERATIVO")
+    console.log("1 - Soma")
+    console.log ("2 - subtração ")
+    console.log ("3 - Multiplicação ")
+    console.log ("4 - divisao")
+    
+    opcao = Number(prompt("Escolha um recurso: "))
+    
 switch(opcao){
     case 1:
         console.log(`O resultado é ${n1+n2}`);
@@ -21,11 +29,15 @@ switch(opcao){
     case 4:
          console.log(`O resultado é ${n1/n2}`)
          break;
+         
+    case 5:
+        console.log ("Obrigado por usar nossa calculadora!")
+        break;
 
     default:
         console.log("Operação inválida")
 }
-
+} while (opcao !== 5);
 
 
 
