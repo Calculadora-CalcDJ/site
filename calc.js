165440e8fbbe72df69794153f79d73cf08d60d25
Débora Luiza de Paula Ferreira <163172676+d3bLuiz4@users.noreply.github.com>
2025-06-22 01:11:53 -0300

    ajustes finais calc.js

--- calc.js
+++ calc.js
@@ -1,92 +1,34 @@
-console.log("Seja bem vindo!!!") 
+let display = document.getElementById("res");
+let botoes = document.querySelectorAll(".bot");
 
-Function somar(numeros){
-    let resultado = 0
-    for (let x = 0; x < numeros.length; x++) {
-            resultado += numeros[x] 
-    }
-    return resultado;
-}
+let expressao = "";
 
-Function subtrair(numeros){
-    let resultado = numeros[0]
-    for (let x = 1; x < numeros.length; x++) {
-            resultado -= numeros[x] 
-    }
-    return resultado;
-}
+botoes.forEach(function(botao) {
+    botao.addEventListener("click", function() {
+        let valor = botao.innerText;
 
-Function multiplicar(numeros){
-    let resultado = 0
-    for (let x = 0; x < numeros.length; x++) {
-            resultado *= numeros[x] 
-    }
-    return resultado;
-}
-
-Function dividir(numeros){
-    let resultado = numeros[0]
-    for (let x = 1; x < numeros.length; x++) {
-            resultado /= numeros[x] 
-    }
-    return resultado;
-}
-
-let opcao;
-
-do{
-    
-    console.log("MENU INTERATIVO")
-    console.log("1 - Soma")
-    console.log ("2 - Subtração ")
-    console.log ("3 - Multiplicação ")
-    console.log ("4 - Divisao")
-    console.log("5 - Sair")
-    
-    opcao = Number(prompt("Escolha um recurso: "))
-    
-    let lista;
-    let numeros;
-    let resultado;
-    
-switch(opcao){
-    case 1: {
-        let lista = prompt("Digite dois ou mais números para somar: ")
-        numeros = lista.split(" ").map(Number);
-        resultado = somar(numeros);
-        console.log(`O resultado da soma dos números escolhidos é: ${resultado }`);
-        break;
-    }
-    
-    case 2: {
-        let lista = prompt("Digite dois ou mais números para subtrair: ")
-        numeros = lista.split(" ").map(Number);
-        resultado = subtrair(numeros);
-        console.log(`O resultado da subtração dos números escolhidos é: ${resultado }`);
-        break;
-    }
-    
-    case 3: {
-        let lista = prompt("Digite dois ou mais números para multiplicar: ")
-        numeros = lista.split(" ").map(Number);
-        resultado = multiplicar(numeros);
-        console.log(`O resultado da multiplicação dos números escolhidos é: ${resultado }`);
-        break;
-    }
-
-    case 4: {
-        let lista = prompt("Digite dois ou mais números para dividir: ")
-        numeros = lista.split(" ").map(Number);
-        resultado = dividir(numeros;)
-        console.log(`O resultado da divisão números escolhidos é: ${resultado }`);
-        break;
-    }
-         
-    case 5:
-        console.log ("Obrigado por usar nossa calculadora!")
-        break;
-
-    default:
-        console.log("Operação inválida")
-}
-} while (opcao !== 5);
+        if (valor === "C") {
+            expressao = "";
+            display.innerText = "Digite...";
+        }
+        else if (valor === "<|") {
+            expressao = expressao.slice(0, -1);
+            display.innerText = expressao || "Digite...";
+        }
+        else if (valor === "=") {
+            try {
+                let expressaoAjustada = expressao.replace(/x/g, "*").replace(/,/g, ".");
+                let resultado = eval(expressaoAjustada);
+                display.innerText = resultado;
+                expressao = resultado.toString();
+            } catch (error) {
+                display.innerText = "Erro!";
+                expressao = "";
+            }
+        }
+        else {
+            expressao += valor;
+            display.innerText = expressao;
+        }
+    });
+});
